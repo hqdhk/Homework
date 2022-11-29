514bdcab7bb7b905f6486fb7d94a4df79d1bdc50
Евгений Королёв <ytrokinterteiment@gmail.com>
2022-11-29 09:37:58 +0100

    solve second task

--- homework.py
+++ homework.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+# 11 -> 22
+# 188 -> 191
+# 191 -> 202
+# 2541 -> 2552
+
+def ispalindrom(number: int) -> int:
+    for el in range(number + 1, number + 50):
+        reverse_num = int("".join(list(str(el)[::-1])))
+        if el == reverse_num:
+            return el
+
+
+
+
+
+
+
+
+
